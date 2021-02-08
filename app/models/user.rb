424c8e16a59cda71f766ca0dff03518c9c8b9caf
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-08 18:37:13 +0900

    バリーデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: ture
+  validates :first_name, presence: ture
+  validates :last_name, presence: ture
+  validates :first_name_kana, presence: ture
+  validates :last_name_kana, presence: ture
+  validates :birth_day, presence: ture
+
 end
