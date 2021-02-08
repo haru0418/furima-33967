4971b88140081123465839cbf814249c356346e1
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-08 18:40:47 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: ture
-  validates :first_name, presence: ture
-  validates :last_name, presence: ture
-  validates :first_name_kana, presence: ture
-  validates :last_name_kana, presence: ture
-  validates :birth_day, presence: ture
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birth_day, presence: true
 
 end
