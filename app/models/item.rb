24b103e56bbc5e46a90ef11148a979e86d84fcf9
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-10 14:39:51 +0900

    バリデーションとアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+
+  validates :product, presence: true
+  validates :product_description, presence: true
+  validates :category_id, presence: true
+  validates :product_condition_id, presence: true
+  validates :ship_burden_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :ship_day_id, presence: true
+  validates :price, presence: true
+
+  belongs_to :user
 end
