e61bf5cdcc862113fca77df8c5c105d5c8681810
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-12 11:38:42 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,14 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :product, presence: true
   validates :product_description, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1 }
-  validates :product_condition_id, presence: true, numericality: { other_than: 1 }
-  validates :ship_burden_id, presence: true, numericality: { other_than: 1 }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
-  validates :ship_day_id, presence: true, numericality: { other_than: 1 }
-  validates :price, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: 'status Select' }
+  validates :product_condition_id, presence: true, numericality: { other_than: 1, message: 'status Select' }
+  validates :ship_burden_id, presence: true, numericality: { other_than: 1, message: 'status Select' }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: 'status Select' }
+  validates :ship_day_id, presence: true, numericality: { other_than: 1, message: 'status Select' }
+  validates :price,presence: true, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
 
+  
   belongs_to :user
   has_one_attached :image
 
