a6bcb2bb52c47e5d87c613f89b69121c134d611a
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-12 15:38:57 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,17 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :product, presence: true, length: { maximum: 40 }
   validates :product_description, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true, numericality: { other_than: 1, message: 'status Select' }
-  validates :product_condition_id, presence: true, numericality: { other_than: 1, message: 'status Select' }
-  validates :ship_burden_id, presence: true, numericality: { other_than: 1, message: 'status Select' }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: 'status Select' }
-  validates :ship_day_id, presence: true, numericality: { other_than: 1, message: 'status Select' }
   validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
-
+  
+  with_options presence: true, numericality: { other_than: 1, message: 'status Select' } do
+    validates :category_id
+    validates :product_condition_id
+    validates :ship_burden_id
+    validates :prefecture_id
+    validates :ship_day_id
+  end
+  
   belongs_to :user
   has_one_attached :image
 
