768e8047a0bb243851f570c5cc1e4fcfbbc5bd41
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-13 18:15:59 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :orders, only: :index
+    resources :orders, only: :index do
+      resources :addresses, only: :create
+    end
   end
 end
