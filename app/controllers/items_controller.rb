f33d936be4c34fae1cf05bfd4f37741ca93c91b9
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-13 13:51:47 +0900

    ルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,7 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   private
@@ -27,5 +33,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :product, :product_description, :category_id, :product_condition_id, :ship_burden_id,
                                  :prefecture_id, :ship_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
