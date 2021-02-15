08a129db8865919dc00c3d7d18fb3e1ea5bcf44b
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-15 17:50:34 +0900

    バリデーション追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,11 +4,11 @@ class OrderAddress
   attr_accessor :post_code, :prefecture_id, :prefecture_city, :prefecture_block, :prefecture_buildin, :phone_number, :order_id, :user_id, :item_id
   
   with_options presence: true do
-    validates :post_code
+    validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly"}
     validates :prefecture_id, numericality: { other_than: 1, message: 'status Select' }
     validates :prefecture_city
     validates :prefecture_block
-    validates :phone_number
+    validates :phone_number, format: { with: /\A[0-9]+\z/, message: 'Input only number' }
   end
 
   def save
