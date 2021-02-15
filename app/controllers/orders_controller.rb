73d2e37959ab051a243f587368661bc8d321ef4f
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-15 11:49:04 +0900

    コントローラーのcreate記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,23 @@ class OrdersController < ApplicationController
   
   def index
     @item = Item.find(params[:item_id])
+    @orderaddress = OrderAddress.new
   end
 
+  def create
+    @item = Item.find(params[:item_id])
+    @orderaddress = OrderAddress.new(orderaddress_params)
+    if @orderaddress.valid?
+      @orderaddress.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def orderaddress_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :prefecture_city, :prefecture_block, :prefecture_buildin, :phone_number, :order_id, :item_id).merge(user_id: current_user.id)
+  end
 end
