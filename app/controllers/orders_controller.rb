1a9b8812202e362753876a01622d99fe2a898af2
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-16 14:10:14 +0900

    pay.jpに送信する値の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @orderaddress = OrderAddress.new(orderaddress_params)
     if @orderaddress.valid?
+      pay_item
       @orderaddress.save
       redirect_to root_path
     else
@@ -19,6 +20,15 @@ class OrdersController < ApplicationController
   private
 
   def orderaddress_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :prefecture_city, :prefecture_block, :prefecture_buildin, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_address).permit(:post_code, :prefecture_id, :prefecture_city, :prefecture_block, :prefecture_buildin, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: orderaddress_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
